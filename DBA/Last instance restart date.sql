811f486c7c059bdb05d7fec1c6112a0db63905cf
IVReddy81 <venkatareddyilluri81@gmail.com>
2023-06-28 12:26:20 +0530

    deleted DBA files

--- DBA/Last instance restart date.sql
+++
@@ -1,6 +0,0 @@
--- Last instance restart date
--- Part of the SQL Server DBA Toolbox at https://github.com/DavidSchanzer/Sql-Server-DBA-Toolbox
--- This script returns the date and time at which this instance was last started
-
-SELECT	[sqlserver_start_time] AS [LastStartupDate]
-FROM	[sys].[dm_os_sys_info]
