f8cf8c92d4a4df444e2b86df7209787421b2d08b
IVReddy81 <venkatareddyilluri81@gmail.com>
2023-06-28 12:23:34 +0530

    all sql files deleted

--- log file space.sql
+++
@@ -1,9 +0,0 @@
-select * from sys.dm_db_log_space_usage
-select * from sys.dm_db_log_info(6)
-select * from sys.dm_db_log_stats(6)
-
-dbcc shrinkdatabase([hyuuu],20)
-
-select * from sys.database_files
-select * from sys.databases
-
--- single user multi user.sql
+++
@@ -1,8 +0,0 @@
-USE [master]
-GO
-ALTER DATABASE [AWS] SET  MULTI_USER WITH NO_WAIT
-GO
-
-
-use master
-alter database AWS set single_user
